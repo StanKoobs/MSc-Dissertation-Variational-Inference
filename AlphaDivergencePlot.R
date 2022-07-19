be6feeaa59733ea12c50c911a56baafc3ccc89f7
Stan Koobs <47364394+StanKoobs@users.noreply.github.com>
2022-07-19 19:37:16 +0100

    Add files via upload

---
+++ AlphaDivergencePlot.R
@@ -0,0 +1,109 @@
+### Replicating Figure 1 Minka (2005)
+
+source("Packages.R")
+source("DissertationggTheme.R")
+
+# Point grid
+x <- seq(-1.8, 2, by = 0.01)
+
+# Mixture of Gaussians we will be trying to approximate
+p <- function(y) {
+  0.45 * dnorm(y, -0.5, 0.2) + 0.55 * dnorm(y, 0.5, 0.4)
+}
+
+# Function for alpha divergence for alpha is not zero or one
+falpha <- function(u, alpha) {
+  1 / (alpha * (alpha - 1)) * (u^alpha - 1)
+}
+
+# Alpha-divergence function
+alphadiv <- function(py, qy, alpha) {
+  # qy and py are vectors of probability densities expected to be of the 
+  # same length
+
+  if (length(py) != length(qy)) {
+    stop("py and qy not of equal length")
+  }
+  
+  if (alpha == 0) {
+    #KLD(py, qy)$sum.KLD.py.px
+    sum(qy * (log(qy) - log(py)) + py - qy)
+  } else if (alpha == 1) {
+    #KLD(py, qy)$sum.KLD.px.py
+    sum(py * (log(py) - log(qy))) + sum(qy - py)
+  } else {
+    #sum(falpha(qy / py, alpha = alpha) * py)
+    1 / (alpha * (1 - alpha)) * 
+      sum(alpha * py + (1 - alpha) * qy - py^alpha * qy^(1 - alpha))
+  }
+}
+
+# Now we use q(y) = mass * N(y; mu, sigma) as our variational family
+# Note that mass is a scaler that allows q(y) to be unnormalised
+# This leads to the following objective function
+objective <- function(vec, alpha) {
+  # vec is expected to be 3-dim input vector with mu, sigma, and mass
+  mu <- vec[1]; sigma <- vec[2]; mass <- vec[3]
+  densityp <- p(x)
+  densityq <- mass * dnorm(x, mu, sigma)
+  alphadiv(densityq, densityp, alpha = alpha)
+}
+
+# As it is a simple 1D problem, we use a simple built-in optimisation 
+# algorithm. That is, we use the  method "L-BFGS-B" of Byrd et. al. (1995) 
+# which allows box constraints, that is, each variable can be given a 
+# lower and/or uppe bound.
+findParameters <- function(alpha) {
+  optim(c(0.4, 0.4, 0.6), 
+        objective, 
+        alpha = alpha, 
+        lower = c(-Inf, 0, 0),
+        upper = c(10, 10, 10),
+        method = "L-BFGS-B")$par
+}
+
+# Genrate sequence of py values
+py <- p(x)
+
+# Function to generate desired plots for particular alpha
+generateplot <- function(alpha) {
+  par <- findParameters(alpha)
+  
+  qy <- par[3] * dnorm(x, par[1], par[2]) 
+  
+  # Used for plotting "q" at the right spot in the figure
+  xcoord <- x[which.max(qy)]
+  ycoord <- max(qy)
+  
+  ggplot() +
+    geom_line(aes(x = x, y = py, colour = "py"), size = 1.2) +
+    geom_line(aes(x = x, y = qy, colour = "qy"), size = 1.2) +
+    ggtitle(paste("Alpha = ", alpha)) +
+    theme(plot.title = element_text(hjust = 0.5, size = 16, 
+                                    face = "bold")) +
+    ThesisggTheme() +
+    theme(legend.position="none", 
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          axis.text.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.title.x=element_blank(),
+          axis.title.y=element_blank()) +
+    annotate(geom = "text", x = -0.75, y = 0.9, label = "p",
+             color = "#F8766D", size = 10) +
+    annotate(geom = "text", x = xcoord + 0.3, y = ycoord + 0.02, 
+             label = "q", color = "#00BFC4", size = 10) +
+    ylim(0, 1.02)
+  
+}
+
+# Generate the five figures
+pm20 <- generateplot(-20)
+p0 <- generateplot(0)
+p05 <- generateplot(0.5)
+p1 <- generateplot(1)
+p20 <- generateplot(20)
+
+# Final figure
+plot_grid(pm20, p0, p05, p1, p20, ncol = 5) 
+
--- KLDivergenceIlustration.R
+++ KLDivergenceIlustration.R
@@ -1,15 +1,15 @@
 ### Replicating Figure 10.2 Bishop (2006)
 
+source("Packages.R")
+source("DissertationggTheme.R")
+
 mu <- c(0, 0)
-Sigma <- matrix(c(1, 0.95, 0.95, 1), ncol = 2, nrow = 2)
+Sigma <- matrix(c(1, 0.97, 0.97, 1), ncol = 2, nrow = 2)
 
-#install.packages("mvtnorm")
-library(mvtnorm)
-
-npoints <- length(seq(-3, 3, by = 0.05))
-data <- matrix(c(rep(seq(-3, 3, by = 0.05), npoints), 
-                 rep(seq(-3, 3, by = 0.05), each = npoints)), 
-               ncol = 2)
+x <- seq(-3, 3, by = 0.025)
+npoints <- length(x)
+data <- matrix(c(rep(x, npoints), 
+                 rep(x, each = npoints)), ncol = 2)
 
 # Numerical "brute force" KL minimisation
 
@@ -25,18 +25,9 @@ KL <- function(vec, type = "Forward") {
   }
 }
 
-par <- optim(c(0,0,1,1), KL)$par
+par <- optim(c(0, 0, 0.3, 0.3), KL)$par
 muvar <- par[1:2]
 sigmavar <- diag(par[3:4])
-
-q1 <- qmvnorm(0.6827, mean = mu, sigma = Sigma)
-q2 <- qmvnorm(0.9545, mean = mu, sigma = Sigma)
-q3 <- qmvnorm(0.9973, mean = mu, sigma = Sigma)
-
-
-q1var <- qmvnorm(0.6827, mean = muvar, sigma = sigmavar)
-q2var <- qmvnorm(0.9545, mean = muvar, sigma = sigmavar)
-q3var <- qmvnorm(0.9973, mean = muvar, sigma = sigmavar)
 
 zcondx <- function(x, y) {
   dmvnorm(cbind(x, y), mean = mu, sigma = Sigma)
@@ -45,33 +36,71 @@ variational <- function(x, y) {
   dmvnorm(cbind(x, y), mean = muvar, sigma = sigmavar)
 }
 
-data
 
+findlevelCurves <- function(mu, sigma, func = zcondx) {
+  q1 <- qmvnorm(0.6827, mean = mu, sigma = sigma)$quantile
+  q2 <- qmvnorm(0.9545, mean = mu, sigma = sigma)$quantile
+  q3 <- qmvnorm(0.9973, mean = mu, sigma = sigma)$quantile
+  
+  if (func == "zcondx") {
+    c(zcondx(q1, q1), zcondx(q2, q2), zcondx(q3, q3))
+  } else if (func == "variational") {
+    c(variational(q1, q1), variational(q2, q2), variational(q3, q3))
+  }
+}
 
-#install.packages("tidyverse")
-library(tidyverse)
+breaksp <- findlevelCurves(mu, Sigma, "zcondx")
+breaksq <- findlevelCurves(muvar, sigmavar, "variational")
+
 
 data <- as_tibble(data)
 names(data) <- c("x", "y")
-dataz <- data %>% mutate(z = zcondx(x, y), c = as.factor("Conditional"))
-datavar <- data %>% mutate(z = variational(x, y), c = as.factor("Variational"))
+datap <- data %>% mutate(z = zcondx(x, y), c = as.factor("Conditional"))
+dataq <- data %>% mutate(z = variational(x, y), 
+                         c = as.factor("Variational"))
 
 
-library(ggplot2)
+ForwardPlot <- ggplot() +
+  stat_contour(data = datap, aes(x = x, y = y, z = z, colour = "p(Z|X)"), 
+               breaks = breaksp, size = 1) +
+  stat_contour(data = dataq, aes(x = x, y = y, z = z, colour = "q(z)"),
+               breaks = breaksq, size = 1) +
+  labs(color = "Distribution") + ThesisggTheme() +
+  ggtitle("Forward KL")
+ForwardPlot
 
-ggplot(dataz, aes(x = x, y = y, z = z)) + stat_contour(breaks = c(0.007, 0.096, 0.424))
+### Reverse
 
-ggplot(datavar, aes(x = x, y = y, z = z)) + stat_contour(breaks = c(0.0002, 0.03, 0.677))
+x <- seq(-4.25, 4.25, by = 0.025)
+npoints <- length(x)
+data <- matrix(c(rep(x, npoints), 
+                 rep(x, each = npoints)), ncol = 2)
+
+par <- optim(c(0, 0, 1, 1), KL, type = "Reverse")$par
+muvar <- par[1:2]
+sigmavar <- diag(par[3:4])
+
+breaksp <- findlevelCurves(mu, Sigma, "zcondx")
+breaksq <- findlevelCurves(muvar, sigmavar, "variational")
+
+data <- as_tibble(data)
+names(data) <- c("x", "y")
+datap <- data %>% mutate(z = zcondx(x, y))
+dataq <- data %>% mutate(z = variational(x, y))
 
 
-ggplot() +
-  stat_contour(data = dataz, aes(x = x, y = y, z = z, colour = "blue"), breaks = c(0.007, 0.096, 0.424)) +
-  stat_contour(data = datavar, aes(x = x, y = y, z = z), breaks = c(0.0002, 0.03, 0.677))
+ReversePlot <- ggplot() +
+  stat_contour(data = datap, aes(x = x, y = y, z = z, colour = "p(Z|X)"), 
+               breaks = breaksp, size = 1) +
+  stat_contour(data = dataq, aes(x = x, y = y, z = z, colour = "q(z)"), 
+               breaks = breaksq, size = 1) +
+  labs(color = "Distribution") + ThesisggTheme() +
+  ggtitle("Reverse KL")
+ReversePlot
 
-bind_rows(dataz, datavar) %>%
-  ggplot(aes(x = x, y = y, z = z, colour = c)) + stat_contour(breaks = c(0.007, 0.096, 0.424))
-  
+plot_grid(ForwardPlot, ReversePlot)
 
 
-p = ggplot(data, aes(x = x, y = y, z = qvar)) + geom_contour_filled()
+
+
 
